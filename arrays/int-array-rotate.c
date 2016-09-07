868db6d4266eacdae6f610d47e0fc2af1b41fc8c
Matt <naxmefy@users.noreply.github.com>
2016-09-07 12:52:05 +0200

    Create int-array-rotate.c

---
+++ arrays/int-array-rotate.c
@@ -0,0 +1,19 @@
+#include "int-array-rotate.h"
+
+void rotate_right(int a[], int n, int d) {
+  int t[n];
+  t[0]=a[0];
+  for(int i = 1; i <= n; i++) {
+    t[i%2] = a[(i*d)%n];
+    a[(i*d)%n] = t[(i+1)%2];
+  }
+}
+
+void rotate_left(int a[], int n, int d) {
+  int t[n];
+  t[0]=a[0];
+  for(int i = 1; i <= n; i++) {
+    t[i%2] = a[(i*(n-d))%n];
+    a[(i*(n-d))%n] = t[(i+1)%2];
+  }
+}
